043293fcc1690db4f2ceb66ec70bd7c872854ebb
mahdi <navidi.mahdi@gmail.com>
2025-04-08 12:59:18 +0330

    wip

--- check_ip.py
+++ check_ip.py
@@ -5,5 +5,7 @@ try:
     with urlopen('https://api.ipify.org', timeout=30) as response:
         ip = response.read().decode('utf-8').strip()
         print("External IP:", ip)
+        with open('ip.txt', 'w') as file:
+            file.write(ip)
 except URLError as e:
     print("Error fetching IP:", e)
